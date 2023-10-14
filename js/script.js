84174716c149e96d6434855439045e14a3c31e51
megasenaminha <147941747+megasenaminha@users.noreply.github.com>
2023-10-14 14:54:09 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,164 +1,154 @@
-// mini banco de dados com 10 últimos jogos
-const algunsJogos = [];
+// let dadosDaMegaSena;
 
-// função para adicionar novo jogo
-function adicionarJogo(dataSorteio, numeros) {
-	const jogo = {
-		dataSorteio: dataSorteio,
-		numeros: numeros,
-	};
-	algunsJogos.push(jogo);
-}
+// async function buscarDadosMegaSena() {
+// 	try {
+// 		const resposta = await fetch(
+// 			"https://loteriascaixa-api.herokuapp.com/api/megasena"
+// 		);
 
-// adicionando os últimos 10 jogos
-adicionarJogo(2638, [9, 30, 34, 44, 54, 55]);
-adicionarJogo(2637, [1, 2, 10, 32, 34, 59]);
-adicionarJogo(2636, [5, 16, 38, 42, 43, 48]);
-adicionarJogo(2635, [6, 11, 29, 37, 56, 58]);
-adicionarJogo(2634, [8, 27, 28, 32, 48, 56]);
-adicionarJogo(2633, [2, 23, 25, 33, 45, 54]);
-adicionarJogo(2632, [5, 10, 27, 38, 56, 57]);
-adicionarJogo(2631, [14, 26, 36, 39, 50, 53]);
-adicionarJogo(2630, [14, 18, 22, 26, 31, 38]);
-adicionarJogo(2629, [11, 32, 35, 40, 41, 48]);
-adicionarJogo(2628, [5, 14, 32, 40, 53, 54]);
-adicionarJogo(2627, [13, 25, 31, 43, 57, 58]);
-adicionarJogo(2626, [1, 9, 13, 16, 52, 59]);
-adicionarJogo(2625, [9, 10, 35, 44, 55, 58]);
-adicionarJogo(2624, [5, 31, 37, 47, 52, 58]);
-adicionarJogo(2623, [10, 15, 20, 35, 37, 59]);
-adicionarJogo(2622, [9, 19, 22, 24, 50, 60]);
-adicionarJogo(2621, [6, 9, 14, 16, 42, 47]);
-adicionarJogo(2620, [4, 6, 13, 21, 26, 28]);
-adicionarJogo(2619, [5, 36, 39, 41, 44, 50]);
-adicionarJogo(2618, [6, 17, 29, 35, 45, 48]);
-adicionarJogo(2617, [3, 14, 36, 42, 43, 44]);
-adicionarJogo(2616, [6, 16, 23, 35, 38, 49]);
-adicionarJogo(2615, [5, 7, 22, 23, 41, 59]);
-adicionarJogo(2614, [3, 8, 13, 14, 19, 25]);
-adicionarJogo(2613, [14, 26, 40, 42, 46, 52]);
-adicionarJogo(2612, [20, 27, 34, 44, 50, 54]);
-adicionarJogo(2611, [4, 12, 18, 21, 25, 49]);
-adicionarJogo(2610, [10, 23, 34, 53, 55, 57]);
-adicionarJogo(2609, [3, 21, 27, 32, 35, 60]);
-adicionarJogo(2608, [7, 13, 17, 24, 29, 52]);
-adicionarJogo(2607, [7, 11, 25, 51, 57, 60]);
-adicionarJogo(2606, [2, 10, 16, 32, 45, 49]);
-adicionarJogo(2605, [5, 11, 26, 35, 46, 54]);
-adicionarJogo(2604, [16, 17, 19, 22, 46, 57]);
-adicionarJogo(2603, [3, 7, 13, 29, 52, 56]);
-adicionarJogo(2602, [11, 14, 16, 30, 32, 46]);
-adicionarJogo(2601, [3, 8, 34, 40, 44, 55]);
-adicionarJogo(2600, [4, 18, 37, 38, 46, 60]);
-adicionarJogo(2599, [23, 28, 34, 43, 47, 60]);
-adicionarJogo(2598, [7, 14, 24, 53, 58, 60]);
-adicionarJogo(2597, [14, 26, 34, 54, 56, 58]);
-adicionarJogo(2596, [34, 35, 39, 47, 51, 56]);
-adicionarJogo(2595, [1, 13, 34, 39, 50, 52]);
-adicionarJogo(2594, [7, 26, 32, 35, 49, 55]);
-adicionarJogo(2593, [10, 14, 17, 25, 32, 39]);
-adicionarJogo(2592, [15, 17, 28, 34, 35, 51]);
-adicionarJogo(2591, [10, 11, 21, 23, 28, 30]);
-adicionarJogo(2590, [2, 12, 28, 36, 43, 48]);
-adicionarJogo(2589, [1, 15, 16, 25, 32, 36]);
-adicionarJogo(2588, [9, 13, 22, 31, 57, 58]);
-adicionarJogo(2587, [5, 10, 11, 22, 23, 37]);
-adicionarJogo(2586, [10, 18, 41, 49, 53, 59]);
-adicionarJogo(2585, [14, 26, 34, 36, 43, 59]);
-adicionarJogo(2584, [1, 5, 12, 36, 53, 55]);
-adicionarJogo(2583, [2, 20, 27, 30, 52, 59]);
-adicionarJogo(2582, [10, 14, 17, 19, 21, 34]);
-adicionarJogo(2581, [14, 17, 32, 36, 39, 60]);
-adicionarJogo(2580, [3, 4, 13, 29, 36, 43]);
-adicionarJogo(2579, [5, 10, 26, 35, 38, 44]);
-adicionarJogo(2578, [37, 39, 47, 50, 59, 60]);
-adicionarJogo(2577, [12, 18, 22, 31, 44, 50]);
-adicionarJogo(2576, [29, 32, 33, 35, 38, 43]);
-adicionarJogo(2575, [4, 12, 14, 41, 46, 53]);
-adicionarJogo(2574, [12, 17, 43, 44, 48, 60]);
-adicionarJogo(2573, [6, 26, 32, 35, 37, 49]);
-adicionarJogo(2572, [3, 7, 15, 22, 24, 50]);
-adicionarJogo(2571, [9, 18, 33, 38, 41, 51]);
-adicionarJogo(2570, [8, 18, 26, 27, 47, 50]);
-adicionarJogo(2569, [6, 7, 25, 28, 31, 52]);
-adicionarJogo(2568, [16, 17, 25, 31, 40, 47]);
+// 		dadosDaMegaSena = await resposta.json();
 
-console.log(algunsJogos);
+// 		console.log(dadosDaMegaSena);
+// 	} catch (erro) {
+// 		console.error(erro);
+// 	}
+// }
 
-// Crie um objeto para rastrear a frequência de cada número
-const frequenciaNumeros = {};
+// function usarDados() {
+// 	if (dadosDaMegaSena) {
+// 		console.log(dadosDaMegaSena);
+// 	}
+// }
 
-// Inicialize o objeto frequenciaNumeros com zeros para todos os números possíveis
-for (let i = 1; i <= 60; i++) {
-	frequenciaNumeros[i] = 0;
-}
+// buscarDadosMegaSena();
 
-// Função para atualizar a frequência de um número
-function atualizarFrequencia(numero) {
-	if (frequenciaNumeros[numero]) {
-		frequenciaNumeros[numero]++;
-	} else {
-		frequenciaNumeros[numero] = 1;
+async function buscarResultadosMegaSena() {
+	try {
+		const resposta = await fetch(
+			"https://loteriascaixa-api.herokuapp.com/api/megasena"
+		);
+
+		if (resposta.status === 200) {
+			const dados = await resposta.json();
+			return dados;
+		} else {
+			console.error("Falha ao buscar resultados da Mega Sena.");
+		}
+	} catch (erro) {
+		console.error("Erro na solicitação:", erro);
 	}
 }
 
-// Calcular a frequência de cada número nos jogos
-for (const jogo of algunsJogos) {
-	for (const numero of jogo.numeros) {
-		atualizarFrequencia(numero);
-	}
-}
+function calcularFrequenciaNumeros(dados) {
+	const todasDezenas = [];
 
-console.log(frequenciaNumeros);
-
-// Ordenar os números com base nas frequências em ordem decrescente
-const numerosOrdenados = Object.keys(frequenciaNumeros).sort((a, b) => {
-	return frequenciaNumeros[b] - frequenciaNumeros[a];
-});
-
-// Inicializar grupos A a J
-const grupos = {
-	A: [],
-	B: [],
-	C: [],
-	D: [],
-	E: [],
-	F: [],
-	G: [],
-	H: [],
-	I: [],
-	J: [],
-};
-
-// Distribuir os números nos grupos com base nas frequências
-let grupoAtual = "A"; // Comece com o grupo A
-for (const numero of numerosOrdenados) {
-	const frequencia = frequenciaNumeros[numero];
-
-	// Verificar se o número já está em um grupo anterior
-	let numeroAtribuido = false;
-	for (let i = "A".charCodeAt(0); i <= grupoAtual.charCodeAt(0); i++) {
-		const grupoAnterior = String.fromCharCode(i);
-		if (
-			grupos[grupoAnterior].length < 6 &&
-			frequencia === frequenciaNumeros[grupos[grupoAnterior][0]]
-		) {
-			grupos[grupoAnterior].push(Number(numero));
-			numeroAtribuido = true;
-			break;
+	for (const objeto of dados) {
+		if (objeto.dezenas && Array.isArray(objeto.dezenas)) {
+			todasDezenas.push(...objeto.dezenas);
 		}
 	}
 
-	// Se o número não foi atribuído a um grupo anterior, coloque-o no grupo atual
-	if (!numeroAtribuido) {
-		grupos[grupoAtual].push(Number(numero));
+	const frequenciaNumeros = {};
+	for (const numero of todasDezenas) {
+		const num = parseInt(numero, 10);
+		if (frequenciaNumeros[num]) {
+			frequenciaNumeros[num]++;
+		} else {
+			frequenciaNumeros[num] = 1;
+		}
 	}
 
-	// Avance para o próximo grupo, se necessário
-	if (grupos[grupoAtual].length === 6) {
-		grupoAtual = String.fromCharCode(grupoAtual.charCodeAt(0) + 1);
+	return frequenciaNumeros;
+}
+
+function ordenarPorFrequencia(frequenciaNumeros) {
+	const numerosOrdenados = Object.keys(frequenciaNumeros).sort((a, b) => {
+		return frequenciaNumeros[b] - frequenciaNumeros[a];
+	});
+	return numerosOrdenados;
+}
+
+function mostrarFrequenciaNumeros(frequenciaNumeros) {
+	console.log("Frequência de cada número:");
+	const numerosOrdenados = ordenarPorFrequencia(frequenciaNumeros);
+	for (const numero of numerosOrdenados) {
+		console.log(`Número ${numero}: ${frequenciaNumeros[numero]} vezes`);
 	}
 }
 
-// Imprimir os grupos
-console.log(grupos);
+function separarNumerosEmGrupos(frequenciaNumeros) {
+	const numerosFrequencia = Object.keys(frequenciaNumeros).map((numero) => ({
+		numero: parseInt(numero, 10),
+		frequencia: frequenciaNumeros[numero],
+	}));
+
+	// Ordene os números por frequência decrescente e, em caso de empate, mantenha a ordem original
+	numerosFrequencia.sort((a, b) => {
+		if (b.frequencia === a.frequencia) {
+			return a.numero - b.numero;
+		}
+		return b.frequencia - a.frequencia;
+	});
+
+	const grupos = {
+		A: [],
+		B: [],
+		C: [],
+		D: [],
+		E: [],
+		F: [],
+		G: [],
+		H: [],
+		I: [],
+		J: [],
+	};
+
+	let grupoAtual = "A";
+	let numerosAdicionados = 0;
+	let numeroGrupoAtual = 0;
+
+	for (const numeroObj of numerosFrequencia) {
+		const numero = numeroObj.numero;
+		const frequencia = numeroObj.frequencia;
+
+		if (
+			numerosAdicionados >= 5 &&
+			numerosAdicionados <= 7 &&
+			numeroGrupoAtual < 10
+		) {
+			// Se o grupo atual já tem entre 5 e 7 números e o limite de grupos não foi atingido
+			const ultimoNumeroGrupo =
+				grupos[grupoAtual][grupos[grupoAtual].length - 1];
+
+			if (frequencia === frequenciaNumeros[ultimoNumeroGrupo]) {
+				// Se o próximo número tem a mesma frequência, adicione-o ao mesmo grupo
+				grupos[grupoAtual].push(numero);
+			} else {
+				// Se o próximo número tem frequência diferente, avance para o próximo grupo
+				grupoAtual = String.fromCharCode(grupoAtual.charCodeAt(0) + 1);
+				numeroGrupoAtual++;
+				grupos[grupoAtual] = [numero]; // Crie um novo grupo com o próximo número
+			}
+		} else {
+			grupos[grupoAtual].push(numero);
+		}
+
+		numerosAdicionados++;
+	}
+
+	return grupos;
+}
+
+async function main() {
+	const dados = await buscarResultadosMegaSena();
+
+	if (dados) {
+		const frequenciaNumeros = calcularFrequenciaNumeros(dados);
+		mostrarFrequenciaNumeros(frequenciaNumeros);
+		const gruposDeNumeros = separarNumerosEmGrupos(frequenciaNumeros);
+		console.log("Grupos de números:", gruposDeNumeros);
+	}
+}
+
+// Chame a função principal para buscar os resultados da Mega Sena, calcular a frequência e separar os números em grupos
+main();
